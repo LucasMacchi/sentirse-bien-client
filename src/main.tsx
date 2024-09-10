862d705079ababe491f431c067f1cab0e26ddc8b
paulinadaciuk03 <paulinadaciukmarinoff@gmail.com>
2024-09-10 11:20:47 -0300

    minor details

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import { CssBaseline, ThemeProvider } from '@mui/material'
 import { About } from './Componentes/About/About.tsx'
 import { Servicios } from './Componentes/Servicios/Servicios.tsx'
 import { Contact } from './Componentes/Contact/Contact.tsx'
-import ScrollToTop from './Componentes/ScrollToTop.tsx'
+import ScrollToTop from './Componentes/Others/ScrollToTop.tsx'
 import theme from './Theme/theme.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -24,11 +24,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             <Route path='/' element={<App/>}></Route>
             <Route path='/about' element={<About></About>}></Route>
             <Route path='/contact' element={<Contact></Contact>}></Route>
-            <Route path='/services' element={<Servicios></Servicios>}></Route>
+            <Route path='/services' element={<Servicios></Servicios>}></Route>          
           </Routes>
         </GlobalState>
       </BrowserRouter>
       </ThemeProvider>
     </React.StrictMode>,
   </CssBaseline>
-)
+) 
