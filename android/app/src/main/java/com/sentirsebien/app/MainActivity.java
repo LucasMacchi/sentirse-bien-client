dc40ffc2506c3f77b5d500fbd7c9aa70ff599280
LucasMacchi <lucasmacchi25@gmail.com>
2024-11-11 20:11:08 -0300

    capacitor config

---
+++ android/app/src/main/java/com/sentirsebien/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sentirsebien.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sentirsebien.app',
+  appName: 'sentirse-bien-client',
+  webDir: 'dist'
+};
+
+export default config;
